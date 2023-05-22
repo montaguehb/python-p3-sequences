c9591feb9baa62d788d3fba33c291ac50be9a75f
Historia Montague <historia.montague@outlook.com>
2023-05-22 09:44:03 -0600

    Finished Lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        return print([])
+    elif length == 1:
+        return print([0])
+    fib = [0, 1]
+    for x in range(length - 2):
+        fib.append(fib[-1] + fib[-2])
+    print(fib)
